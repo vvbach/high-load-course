2a12bae449ca154f740adce2cafb5e03787054ec
Andrey Sukhovitsky <Andrey.Sukhovitsky@infobip.com>
2025-06-18 23:58:35 +0300

    experiment

--- src/main/kotlin/ru/quipy/payments/logic/PaymentServiceImpl.kt
+++ src/main/kotlin/ru/quipy/payments/logic/PaymentServiceImpl.kt
@@ -1,27 +1,47 @@
 package ru.quipy.payments.logic
 
+import kotlinx.coroutines.*
 import org.slf4j.LoggerFactory
-import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Service
-import ru.quipy.common.utils.NamedThreadFactory
-import ru.quipy.core.EventSourcingService
-import ru.quipy.payments.api.PaymentAggregate
-import java.time.Duration
 import java.util.*
 import java.util.concurrent.Executors
-import java.util.concurrent.locks.ReentrantLock
-import kotlin.concurrent.withLock
+
 
 
 @Service
 class PaymentSystemImpl(
-    private val paymentAccounts: List<PaymentExternalSystemAdapter>
+    private val paymentAccounts: List<PaymentExternalSystemAdapter>,
+    private val retryQueue: RetryQueue
 ) : PaymentService {
     companion object {
         val logger = LoggerFactory.getLogger(PaymentSystemImpl::class.java)
     }
 
+
+    private val retryQueueWorker = CoroutineScope(Executors.newSingleThreadExecutor().asCoroutineDispatcher()).launch {
+        while (true) {
+            try {
+                val next = retryQueue.getNext()
+                if (next.deadline < System.currentTimeMillis()) {
+                    logger.warn("Payment request deadline expired: {}", next)
+                    continue
+                }
+                submitPaymentRequest(next.paymentId, next.amount, next.paymentStartedAt, next.deadline)
+                logger.info("Retrying: {}", next)
+            } catch (e: Exception) {
+                logger.error("Error while getting next retry payment request", e)
+                delay(1000)
+            }
+        }
+    }.invokeOnCompletion { th -> if (th != null) logger.error("Rate limiter release job completed", th) }
+
+
     override fun submitPaymentRequest(paymentId: UUID, amount: Int, paymentStartedAt: Long, deadline: Long) {
+        if (deadline < System.currentTimeMillis()) {
+            logger.warn("Payment request deadline expired: {}", paymentId)
+            return
+        }
+
         for (account in paymentAccounts) {
             account.performPaymentAsync(paymentId, amount, paymentStartedAt, deadline)
         }
